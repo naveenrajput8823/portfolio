6f721654eb014ea1884d9321798b611c965f6ec0
naveenrajput8823 <naveenrajput3363@gmail.com>
2026-07-08 21:27:01 +0530

    Made portfolio fully responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className="navbar">
 
@@ -9,14 +13,24 @@ function Navbar() {
         <h2>NAVEEN</h2>
       </div>
 
-      <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#education">Education</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#certificates">Certificates</a></li>
-        <li><a href="#contact">Contact</a></li>
+      {/* Hamburger Icon */}
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+
+        <li><a href="#home" onClick={closeMenu}>Home</a></li>
+        <li><a href="#about" onClick={closeMenu}>About</a></li>
+        <li><a href="#skills" onClick={closeMenu}>Skills</a></li>
+        <li><a href="#education" onClick={closeMenu}>Education</a></li>
+        <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+        <li><a href="#certificates" onClick={closeMenu}>Certificates</a></li>
+        <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
+
       </ul>
 
     </nav>
